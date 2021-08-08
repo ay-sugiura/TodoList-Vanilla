20ce291fd5e38a7b7f94a659b205f0ee174227c8
ay-sugiura <65064434+ay-sugiura@users.noreply.github.com>
2021-08-08 22:30:35 +0900

    追加・削除を実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  // li.innerText = inputText;
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.innerText = inputText;
+
+  // button（完了）生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)　生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //削除処理
+    //押された削除ボタンの親タグ（li）を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode.parentNode;
+    //未完了リストから削除
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  //divにボタンをつける
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //liの下にdivを入れる。
+  li.appendChild(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+
+  //   <li>
+  //   <div class="list-row">
+  //     TODOです
+  //     <button>完了</button>
+  //     <button>削除</button>
+  //   </div>
+  // </li>
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
